53a4e81e71c74d05d6d06f149a8c9bf9ec599211
tkwb54 <kawabe_t@worksap.co.jp>
2015-08-06 20:41:12 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
 
     it "shold have the ritht title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
@@ -24,7 +24,7 @@ describe "Static pages" do
     
     it "shold have the ritht title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
   end
 
@@ -37,7 +37,7 @@ describe "Static pages" do
 
     it "shold have the ritht title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
